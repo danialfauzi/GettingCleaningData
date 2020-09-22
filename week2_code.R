d2d2c209f1d43f7f217fa89419c6e0aef987eb59
Danial <danialfauzi@outlook.com>
2020-09-22 12:06:34 +0700

    update RMySQL

--- week2_code.R
+++ week2_code.R
@@ -3,3 +3,11 @@ install.packages("DBI"); library(DBI)
 ucscDb <- dbConnect(MySQL(),user="genome", host = "genome-mysql.cse.ucsc.edu")
 result <- dbGetQuery(ucscDb, "show databases;"); dbDisconnect(ucscDb);
 result
+
+hg19 <- dbConnect(MySQL(),user="genome", db="hg19",
+                  host = "genome-mysql.cse.ucsc.edu")
+allTables <- dbListTables(hg19)
+length(allTables)
+allTables[1:5]
+dbListFields(hg19, "affyU133Plus2")
+count.fields(*)
